ee22d1fc1d3665bbb54db5044a9d93be40a45a12
Preinel <preinelabreu@yahoo.com>
2026-02-15 18:21:13 +0530

    Add files via upload
    Using numpy, pandas, seaborn, matplotlib and scikit-learn

---
+++ project.py
@@ -0,0 +1,69 @@
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv("https://raw.githubusercontent.com/Preinel/Eco-Friendly-Energy-Usage-Prediction-Project/refs/heads/main/smart_home_energy_consumption_large.csv")
+print("Dataset Loaded Successfully\n")
+print(df.head())
+print("\nDataset Info:")
+print(df.info())
+
+df["DateTime"] = pd.to_datetime(df["Date"] + " " + df["Time"])
+
+df["Hour"] = df["DateTime"].dt.hour
+df["Day"] = df["DateTime"].dt.day
+df["Month"] = df["DateTime"].dt.month
+df["Weekday"] = df["DateTime"].dt.weekday
+df["Is_Weekend"] = df["Weekday"].apply(lambda x: 1 if x >= 5 else 0)
+
+df.drop(columns=["Date", "Time", "DateTime"], inplace=True)
+
+print("\nAfter Feature Engineering:")
+print(df.head())
+
+#distribution plot graph
+plt.figure()
+sns.histplot(df["Energy Consumption (kWh)"], kde=True)
+plt.title("Energy Consumption Distribution")
+plt.show()
+
+# boxplot by season
+plt.figure()
+sns.boxplot(x="Season", y="Energy Consumption (kWh)", data=df)
+plt.title("Energy Consumption by Season")
+plt.show()
+
+# lineplot graph by hour
+plt.figure()
+sns.lineplot(x="Hour", y="Energy Consumption (kWh)", data=df)
+plt.title("Energy Consumption by Hour")
+plt.show()
+
+# heatmap
+plt.figure()
+sns.heatmap(df.select_dtypes(include=np.number).corr(), cmap="coolwarm")
+plt.title("Correlation Heatmap")
+plt.show()
+#regression models 
+print("\nModel Selection:")
+print("- Linear Regression")
+print("- Decision Tree Regressor")
+print("- Random Forest Regressor")
+print("- Gradient Boosting Regressor")
+
+df_encoded = pd.get_dummies(df, columns=["Appliance Type", "Season"], drop_first=True)
+
+# Define Features and Target
+X = df_encoded.drop(columns=["Energy Consumption (kWh)"])
+y = df_encoded["Energy Consumption (kWh)"]
+
+# train test split
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+print("Training Samples:", X_train.shape)
+print("Testing Samples:", X_test.shape)
+
